37cc6a12337bba2f7b5c6608477c450a61bb484d
Trevor White <trevor.white@wayne.edu>
2020-12-31 21:45:48 -0500

    Minor logging update

--- ContestBot/main.py
+++ ContestBot/main.py
@@ -14,8 +14,7 @@ def main():
         if tweets:
             for tweet in tweets:
                 tweet_num += 1
-                logger.info("\n")
-                logger.info("--------------------------------------------------")
+                logger.info("\n--------------------------------------------------")
                 logger.info(f'Tweet number: {tweet_num}')
                 logger.info("--------------------------------------------------")
                 tweet_text = bot.check_tweet(logger, api, tweet)
